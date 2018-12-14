8c2ad2207dc798ffa29b33c7bf8eef91b5e99b25
HardCarroll <phanchan_hc@163.com>
2018-12-14 21:54:26 +0800

    edit @12/14

--- lib/php/handle.php
+++ lib/php/handle.php
@@ -6,20 +6,23 @@ if (!isset($_SESSION["bFirst"]) || empty($_SESSION["bFirst"])) {
 }
 
 if (isset($_POST["token"]) && !empty($_POST["token"])) {
+  // 首页巨幕按钮
   if("indexPage" === $_POST["token"]) {
     $_SESSION["bFirst"] = true;
     echo "#".$_POST["token"];
   }
+  
+  // 免费咨询按钮
   if("consult" === $_POST["token"]) {
     $type = $_POST["type"];
     $area = $_POST["area"];
     $phone = $_POST["phone"];
-    if("canteen" === $type) {
-      $ret = '{"err_code": "ok"}';
-    }
-    else {
-      $ret = '{"type": "'.$type.'", "area": "'.$area.'", "phone": "'.$phone.'"}';
-    }
+    /**
+     * $err_no=0, success
+    */
+    $err_no = 0;
+    $err_code = "您的资料已成功提交，稍后将会有客服人员联系您，请保证电话通畅。";
+    $ret = '{"err_no":'.$err_no.', "err_code":"'.$err_code.'"}';
     echo json_encode($ret);
   }
 }
